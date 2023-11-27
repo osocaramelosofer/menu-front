02c1a8bbaf859dd0ddbc531ba469200e73976a1a
Fernando Miguel <miguel@itnora.com>
2023-11-27 10:55:48 -0600

    add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@nextui-org/button'
 
-import {Button} from '@nextui-org/button';
-
-export default function Home() {
+export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button color='primary'>Click me!</Button>
+
+      <h1 className='hola'>welcome</h1>
     </main>
   )
 }
