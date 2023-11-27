f2914f8dce45815d528fdf7a756c6d04e44c67bb
Fernando Miguel <miguel@itnora.com>
2023-11-27 11:41:44 -0600

    update eslint rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from '@nextui-org/button'
 
 export default function Home () {
+  const name = 'fernando'
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button color='primary'>Click me!</Button>
+      <Button color="primary">Click me!</Button>
 
-      <h1 className='hola'>welcome</h1>
+      <h1 className="text-red-200 hover:scale-75">welcome</h1>
     </main>
   )
 }
