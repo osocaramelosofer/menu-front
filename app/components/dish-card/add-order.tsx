39edbc16dfafeb2a4f2ed66fd6d9f947d7670b54
Fernando Miguel <miguel@itnora.com>
2023-12-04 22:47:47 -0600

    add dish card component

---
+++ app/components/dish-card/add-order.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from '@nextui-org/react'
+import { FaRegEdit } from 'react-icons/fa'
+
+export default function AddOrderButton () {
+  return (
+    <Button size='sm' className='bg-orange-600 text-white text-[8px] h-[21px] mt-2'>
+        Add to order
+        <FaRegEdit />
+    </Button>
+  )
+}
---
+++ lib/dummyDishes.ts
@@ -0,0 +1,40 @@
+import { type IDishCardProps } from '@/app/components/dish-card/dish-card'
+
+export const dishesData: IDishCardProps[] = [
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  },
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  },
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  },
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  },
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  },
+  {
+    srcImage: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sushi-empanizado/main-header.jpg',
+    alt: 'sushi roll',
+    dishName: 'breaded sushi',
+    description: 'Short grain rice, rice vinegar, nori seaweed, blanched medium shrimp'
+  }
+]
